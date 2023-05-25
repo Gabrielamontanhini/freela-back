0fb82a3461d01b1be06b2dbd59ce00e72b91a59a
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-05-24 22:38:55 -0300

    Feature: autenticação com token e hash de senha

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import pg from "pg"
+import bcrypt from "bcrypt"
+import {v4 as uuid} from "uuid"
 
 //Estrutura em Minimum Viable Product 
 
@@ -21,6 +23,60 @@ const configDatabase = {
 const db = new Pool(configDatabase);
 
 //Endpoints
+app.get("/teste", async (req, res) => {
+    try {
+        const teste = await db.query(`SELECT * FROM usuarios;`)
+        res.send(teste.rows)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/cadastro", async (req, res) => {
+    const { nome, email, foto, biografia, senha } = req.body
+    try {
+
+        const hash = bcrypt.hashSync(senha, 10)
+        await db.query(`INSERT INTO usuarios
+    (nome, email, foto, biografia, senha)
+    VALUES
+    ($1, $2, $3, $4, $5);`, [nome, email, foto, biografia, hash])
+        return res.sendStatus(201)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/entrar", async (req, res) => {
+    const { email, senha } = req.body
+    try {
+        
+        const usuario = await db.query(`SELECT * FROM usuarios WHERE email = $1;`, [ email])
+        if (usuario.rowCount==0) return res.sendStatus(404)
+        const hash = bcrypt.hashSync(senha, 10)
+        const senhaCorreta = bcrypt.compare(hash, usuario.rows[0].senha)
+        if (!senhaCorreta) return res.status(401).send({ message: "Senha incorreta!" })
+        const token = uuid()
+
+        return res.status(201).send({token})
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+
+
+app.get("/post", async (req, res) => {
+    try {
+        const teste = await db.query(`SELECT posts.* , usuarios.nome, usuarios.foto FROM posts
+        JOIN usuarios ON posts.postador = usuarios.id;`)
+        res.send(teste.rows)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
 
 // Deixa o app escutando, à espera de requisições
 const PORT = 5000
